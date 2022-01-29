663877be13a28e2204310fae47884b4f8c859127
LoganHoogendijk <logan.hoogendijk@gmail.com>
2022-01-29 18:07:50 -0500

    tested arrays in array for instructions on cook page

--- src/views/Cook.jsx
+++ src/views/Cook.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { selectCookingReceipe } from "../features/receipeSlice";
 import { AiOutlineCheckSquare } from "react-icons/ai";
@@ -6,13 +6,51 @@ import { AiOutlineCheckSquare } from "react-icons/ai";
 function Cook() {
   const cookingReceipe = useSelector(selectCookingReceipe);
   const { name, image } = cookingReceipe;
-  const [instructionBatch, setInstructionBatch] = useState([
+  // use displayList array that only stores 5 strings and update it each time next step or previous step is clicked
+  const displayList = [];
+  const [instructionBatch, setInstructionBatch] = useState([]);
+  const data = [
     "Put tomato in oven",
     "Salt Garlic",
     "Put rice in microwave",
     "Put tomato in oven",
     "Put rice in microwave",
-  ]);
+    "2Put tomato in oven",
+    "2Salt Garlic",
+    "2Put rice in microwave",
+    "2Put tomato in oven",
+    "2Put rice in microwave",
+    "3Put tomato in oven",
+    "3Salt Garlic",
+    "3Put rice in microwave",
+    "3Put tomato in oven",
+    "3Put rice in microwave",
+  ];
+
+  const formatBatch = () => {
+    let tempArray = [];
+    let tempInArray = [];
+
+    for (let i = 0; i < data.length; i++) {
+      //So if we have 5 instructions we add the array into array [[1,2,3,5],]
+      if (i!==0 && i % 5 === 0) {
+        tempArray.push(tempInArray);
+        tempInArray = [];
+      }
+      //Checks if we've reached end of the array to store the last batch.
+      if (i === data.length - 1) {
+        tempArray.push(tempInArray);
+      }
+
+      //Fills up the temp batch with the 5 instructions
+      tempInArray.push(data[i]);
+    }
+  };
+
+  useEffect(() => {
+    formatBatch();
+  }, []);
+
   return (
     <div className="cook-container">
       <div className="cook-header">
@@ -21,7 +59,6 @@ function Cook() {
             <h1 className="cook-receipe-name">{name}</h1>
             <h3 className="instructions-title">Instructions</h3>
             <div className="instruction-batch">
-
               {instructionBatch.map((step) => (
                 <div className="instruction-item">
                   <input type="checkbox" className="instruction-checkbox" />
